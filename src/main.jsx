7500d7ca02ef9445a536b8de3361ced7aca9a41f
OtmaneTouhami <otmanetouhami01@gmail.com>
2023-10-22 21:42:28 +0100

    Replace Browser router with hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/HammerRock/"}>
+        {/* <BrowserRouter basename={import.meta.env.DEV ? "/" : "/HammerRock/"}> */}
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
+        {/* </BrowserRouter> */}
     </React.StrictMode>
 );
