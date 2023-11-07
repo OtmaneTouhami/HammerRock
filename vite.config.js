203e61dc5596e961c32dca24f348c6ca6840f614
OtmaneTouhami <otmanetouhami01@gmail.com>
2023-11-07 19:24:40 +0100

    Re-configure workflow

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
     const config = {
         plugins: [react()],
-        base: "/",
+        base: "/HammerRock/",
     };
 
     return config;
