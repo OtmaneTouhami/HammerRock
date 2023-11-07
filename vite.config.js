a44529eae473dd439941d34a02faa424f7ce41ec
OtmaneTouhami <otmanetouhami01@gmail.com>
2023-11-07 17:58:36 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
     const config = {
         plugins: [react()],
         base: "/",
     };
-
-    if (command !== "serve") {
-        config.base = "/HammerRock/";
-    }
 
     return config;
 });
